91b3163bfc4ba5edb2729e6098970684eddc0bb2
m <mathsec53@gmail.com>
2025-05-15 18:51:24 -0300

    ccs-additions/sintax-fix

--- www/js/controllers/pagesControllers/homeController.js
+++ www/js/controllers/pagesControllers/homeController.js
@@ -1,2 +1,14 @@
-app.controller('homeController', function($scope) {
+app.controller('homeController', function($scope, $interval) {
+    $scope.placeholders = [
+    'https://open.spotify.com/track/...',
+    'https://open.spotify.com/album/...'
+  ];
+  $scope.currentPlaceholder = $scope.placeholders[0];
+  $scope.placeholderIndex = 0;
+
+
+  $interval(function() {
+    $scope.placeholderIndex = ($scope.placeholderIndex + 1) % $scope.placeholders.length;
+    $scope.currentPlaceholder = $scope.placeholders[$scope.placeholderIndex];
+  }, 3000)
 });
